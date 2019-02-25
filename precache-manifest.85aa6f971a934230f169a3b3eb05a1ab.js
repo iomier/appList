b5cb3d663b4b0fe87c86426790a07a87128f956a
iomi <omidi.iman@gmail.com>
2019-02-26 01:11:13 +0330

    Updates

---
+++ precache-manifest.85aa6f971a934230f169a3b3eb05a1ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29281bf5fa6a3ff674d7",
+    "url": "/appList/static/css/main.945f22bd.chunk.css"
+  },
+  {
+    "revision": "29281bf5fa6a3ff674d7",
+    "url": "/appList/static/js/main.29281bf5.chunk.js"
+  },
+  {
+    "revision": "dbc18e5cfe2ec714e16a",
+    "url": "/appList/static/js/runtime~main.dbc18e5c.js"
+  },
+  {
+    "revision": "7773daec386e5528d600",
+    "url": "/appList/static/css/2.922e8ca5.chunk.css"
+  },
+  {
+    "revision": "7773daec386e5528d600",
+    "url": "/appList/static/js/2.7773daec.chunk.js"
+  },
+  {
+    "revision": "79b17ebe71af4b77063cd98d7bdc8335",
+    "url": "/appList/index.html"
+  }
+];
